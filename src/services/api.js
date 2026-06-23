4bdbb40a22030bfabfa12ed04ecdf5e4a5488bbe
Rahul <rahul@Rahuls-MacBook-Air.local>
2026-06-23 18:36:10 +0530

    hardcode API URL temporarily

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
+baseURL: 'https://chit-fund-management-l6rn.onrender.com/api',
 });
 
 // Chit Group
